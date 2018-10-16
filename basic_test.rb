0a04f00b1a2976c6e828fe8f1d29b2b17a300708
Ayyoub <ayyoubjadoo@gmail.com>
2018-10-16 12:56:04 +0300

    Manual tests for development

---
+++ basic_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require 'byebug'
+require './lib/messaging_adapter.rb'
+
+puts 'Basic Test Started..'
+
+puts MessagingAdapter::MessageBroker.available_adapters
+
+# Use the RMQ adapter
+broker = MessagingAdapter::MessageBroker.new(:RabbitMQ)
+broker.publish('test', "msg 1")
+broker.publish('test_queue', "msg 2", direct_to_q: true)
+broker.subscribe('test_queue') do |m|
+    puts m
+end
+
+# Use another adapter (not implemented yet)
+broker = MessagingAdapter::MessageBroker.new(:Kafka)
+broker.publish('test', "msg 1")
+broker.subscribe('test')
--- test.rb
+++ test.rb
@@ -3,13 +3,14 @@
 require './lib/messaging_adapter.rb'
 
 puts 'Test Started..'
+
 # Use the broker with the default adapter
 broker = MessagingAdapter::MessageBroker.new(:RabbitMQ)
 
 t1 = Thread.new do
-  broker.publish('test_queue', x: 2, y: 5)
+  broker.publish('test', x: 2, y: 5)
   broker.publish('test', x: 29, y: 46)
-  broker.publish('test', x: 34, y: 89)
+  broker.publish('test_queue', {x: 34, y: 89}, direct_to_q: true)
   broker.subscribe('test_queue') do |payload|
     puts "[x] Received message: #{payload}"
   end
@@ -19,22 +20,10 @@ t2 = Thread.new do
   broker.publish('test', x: 200, y: 500)
   broker.publish('test', x: 290, y: 460)
   broker.publish('test', x: 340, y: 890)
-  broker.subscribe('test_queue2') do |payload|
+  broker.subscribe('test_queue2', block: false) do |payload|
     puts "[x] Received message[2]: #{payload}"
   end
 end
 
 t1.join
 t2.join
-
-# # Use it explicitly
-# broker = MessageBroker.new(:RabbitMQ)
-# broker.publish('test', { x: 59, y: 29 }.to_s)
-# broker.subscribe('test_queue')
-
-# # Use another adapter
-# broker = MessageBroker.new(:Kafka)
-# broker.publish('test', { x: 7, y: 9 }.to_s)
-# broker.subscribe('test')
-
-# puts MessageBroker.available_adapters
