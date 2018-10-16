342526167918e74528a06e77db28dfd17668b5a9
Ayyoub <ayyoubjadoo@gmail.com>
2018-10-16 12:32:39 +0300

    Add the options hash param to Kafka skeleton adapter

--- lib/messaging_adapter/adapters/kafka.rb
+++ lib/messaging_adapter/adapters/kafka.rb
@@ -3,12 +3,14 @@
 module MessageBrokerAdapter
   # Adapter implements Apache Kafka
   class Kafka
-    def self.publish(topic, payload)
+    def self.publish(topic, payload, options = {})
       puts "Not implemented yet!! publish{#{topic} [#{payload}]}"
+      puts options
     end
 
-    def self.subscribe(topic)
+    def self.subscribe(topic, options = {})
       puts "Not implemented yet!! subscribe{#{topic}}"
+      puts options
     end
   end
 end
